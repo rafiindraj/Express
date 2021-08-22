f01efe1ff54e4747f6d4ede2bf12956521031db8
Rafi <99.rafiindrajati@gmail.com>
2021-08-23 00:46:05 +0700

    add validator example script

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
+const  validator = require('validator')
+
 const notes = require('./notes')
 const shownotes = notes()
-console.log(shownotes)
+// console.log(shownotes)
 
+console.log(validator.isEmail('rafindraj@gmail.com'))
+console.log(validator.isURL('https://labelioososao.d'))
 // const add = require('./utils')
 
 // const sum = add(4, 2)
-
+ 
 // console.log(sum)
