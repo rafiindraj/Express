d677dacedcd302ca311c635e13c425cc24fdcda2
Rafi Indrajati <99.rafiindrajati@gmail.com>
2021-08-25 12:36:07 +0700

    add yargs

--- app.js
+++ app.js
@@ -1,13 +1,77 @@
-const  validator = require('validator')
-
+const colors = require('chalk')
 const notes = require('./notes')
-const shownotes = notes()
-// console.log(shownotes)
+const yargs = require('yargs')
+const { argv } = require('yargs')
 
-console.log(validator.isEmail('rafindraj@gmail.com'))
-console.log(validator.isURL('https://labelioososao.d'))
-// const add = require('./utils')
+//variable declaration
+const log = console.log
+const processed = process.argv
+// const yargument = yargs.argv
+const command = processed[2]
 
-// const sum = add(4, 2)
- 
-// console.log(sum)
+
+// customize yargs version
+yargs.version('1.1.0')
+
+
+//create add command yargs
+yargs.command({
+    command: 'add',
+    describe : 'Add a note',
+    builder:{
+        title:{
+            describe: 'Note Title',
+            demandOption: true,
+            type: 'string'
+        },
+        contents:{
+            describe: 'Note Contents',
+            demandOption: true,
+            type: 'string'
+        }      
+    },
+    handler: function(){
+        log('Title: ' + argv.title)
+    }
+})
+
+//create remove command yargs
+yargs.command({
+    command: 'remove',
+    describe : 'Remove a note',
+    handler: function(argv){
+        log('Removing a note..', argv)
+    }
+})
+
+//create read command yargs
+yargs.command({
+    command: 'read',
+    describe : 'read a note',
+    handler: function(){
+        log('Reading a note..')
+    }
+})
+
+//create list command yargs
+yargs.command({
+    command: 'list',
+    describe : 'List a note',
+    handler: function(){
+        log('Listing a note..')
+    }
+})
+
+// yargs.parse()
+// log(processed)
+console.log(yargs.argv)
+// if(command == 'add'){
+//     log('Adding Note')
+// } else if(command == 'remove'){
+//     log('Removing Note')
+// }
+// log(shownotes)
+// log(colors.blue.inverse('success'))
+
+// log(process.argv[2])
+
